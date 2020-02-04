2822db9a4dbc7499e5fed5feda5d421a651324cc
OUBAH Said <oubahs@pglw0240.lild01.pictime.fr>
2020-02-04 13:34:14 +0100

    creer et initialiser projet spring boot

---
+++ src/main/java/org/sid/SpringBootDebutantApplication.java
@@ -0,0 +1,22 @@
+package org.sid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.logging.Logger;
+
+@SpringBootApplication
+public class SpringBootDebutantApplication {
+
+    //private static Logger logger = Logger.getLogger("SpringBootDebutantApplication");
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDebutantApplication.class, args);
+
+
+        //logger.info("Configuration initial de mon projet Spring Boot");
+
+
+    }
+
+}
