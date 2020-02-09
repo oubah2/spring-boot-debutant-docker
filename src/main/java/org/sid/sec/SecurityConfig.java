24523fcc8a8bf27108f6a0eb6791efced59d45f3
oubah said <oubahsaid2@gmail.com>
2020-02-09 17:51:10 +0100

    add docker to remote

--- src/main/java/org/sid/sec/SecurityConfig.java
+++ src/main/java/org/sid/sec/SecurityConfig.java
@@ -33,6 +33,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.authorizeRequests().antMatchers("/login/**").permitAll();
+        http.authorizeRequests().antMatchers("/users/**").permitAll();
         http.authorizeRequests().antMatchers("/register/**").hasAuthority("ADMIN");
         http.authorizeRequests().antMatchers("/deleteUser/{id}").hasAuthority("ADMIN");
         http.authorizeRequests().anyRequest().authenticated();
